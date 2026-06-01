f88a161e6ca49a2c5d8b78f866da040810b6478f
MtRocha <132596262+MtRocha@users.noreply.github.com>
2026-05-31 22:57:28 -0300

    fix arqvs

--- RvMoniPy/RvMoniPy/RvMoniPy.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import text
-from sqlalchemy.orm import sessionmaker
-from repositories.db_proc import SessionLocal, get_db
-
-SessionLocal = get_db()
-
-result = SessionLocal.execute(text("SELECT 1")).fetchone()
-print(result)
